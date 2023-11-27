1f7bfa6119f4e474f3fa289431a3460187c5fdb2
vanshita shah <vanshita.s@simformsolutions.com>
2023-11-27 11:16:03 +0530

    fix: fix scrolling behaviour for safari browser

---
+++ omnifood/scripts/index.js
@@ -0,0 +1,60 @@
+const header = document.querySelector(".header");
+
+const menuBtn = document.querySelector(".icon-mobile-nav[name='menu-outline']");
+
+const closeBtn = document.querySelector(
+  ".icon-mobile-nav[name='close-outline']"
+);
+
+// Set valid copyright year
+const yearEl = document.querySelector(".year");
+
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+const openNav = () => {
+  header.classList.toggle("nav-open");
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "block";
+};
+
+const closeNav = () => {
+  header.classList.toggle("nav-open");
+  menuBtn.style.display = "block";
+  closeBtn.style.display = "none";
+};
+
+////////////////////////////////////////////////////
+// Smooth scrolling animation
+///////////////////////////////////////////////////
+
+const allLinks = document.querySelectorAll("a:link");
+
+console.log(allLinks);
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to top
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const element = document.querySelector(href);
+      element.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      header.classList.remove("nav-open");
+  });
+});
